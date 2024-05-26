75bf7e555ddfd5e8beb068dcccfb0582828f32e0
Camille <camille@gmail.com>
2024-05-26 18:21:22 -0300

    projeto updt

---
+++ tcc/app.js
@@ -0,0 +1,115 @@
+// nav toggler 
+
+let navToggler = document.querySelector('.nav-toggler');
+let linksContainer = document.querySelector('.links-container');
+
+navToggler.addEventListener('click', () => {
+  navToggler.classList.toggle('active');
+  linksContainer.classList.toggle('active');
+})
+
+// sobre review slider 
+
+let reviews = document.querySelectorAll('.review-wrapper');
+
+let currentReviews = [0, 2]; 
+
+let updateReviewsSlider = (cards) => {
+
+    cards.forEach((card_index) => {
+        reviews[card_index].classList.add('active'); 
+})
+}
+
+setInterval(() => {
+    currentReviews.forEach((card_index, i) => {
+        reviews[card_index].classList.remove('active');
+
+        currentReviews[i] = card_index >= reviews.length - 1 ? 0 : card_index + 1;
+    })
+
+    setTimeout(() => {
+        updateReviewsSlider(currentReviews);
+    }, 250);
+
+}, 5000);
+
+updateReviewsSlider(currentReviews);
+
+
+// faq 
+
+let faqs = [...document.querySelectorAll('.faq')];
+
+faqs.map(faq => {
+    let ques = faq.querySelector('.question-box');
+
+    ques.addEventListener('click', () => {
+        faq.classList.toggle('active');
+    })
+})
+
+// desconto 
+
+let dishSlider = document.querySelector('.dish-slide');
+
+let rotationVal = 0; 
+
+setInterval(() => {
+   
+    rotationVal += 120;
+
+    dishSlider.style.transform = `translateY(-50%) rotate(${rotationVal}deg)`;
+
+}, 3000)
+
+// go to top button 
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+// loader 
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
+
+// cookies 
+
+var msgCookies = document.getElementById("cookies-msg");
+
+function aceito() {
+  localStorage.lgpd = "sim"; 
+  msgCookies.classList.remove('mostrar'); 
+}
+
+if(localStorage.lgpd == "sim") {
+  msgCookies.classList.remove('mostrar');
+} else{
+  msgCookies.classList.add('mostrar');
+}
+
+AOS.init(); 
